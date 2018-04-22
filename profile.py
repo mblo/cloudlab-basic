1c29dcc8ac53e9a1fb32b28e9c68755304419799
Marcel Blöcher <bloecher@dsp.tu-darmstadt.de>
2018-04-22 10:22:08 +0200

    readme updated

--- profile.py
+++ profile.py
@@ -1,9 +1,5 @@
-"""This is a trivial example of a gitrepo-based profile; The profile source code and other software, documentation, etc. are stored in in a publicly accessible GIT repository (say, github.com). When you instantiate this profile, the repository is cloned to all of the nodes in your experiment, to `/local/repository`. 
-
-This particular profile is a simple example of using a single raw PC. It can be instantiated on any cluster; the node will boot the default operating system, which is typically a recent version of Ubuntu.
-
-Instructions:
-Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
+"""
+This profile uses a git repository based configuration
 """
 
 # Import the Portal object.
@@ -16,7 +12,7 @@ pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
+
 # Add a raw PC to the request.
 node = request.RawPC("node")
 
