2c599b2ea5c067a719c8b17615ea2324389ca443
Marcel Blöcher <bloecher@dsp.tu-darmstadt.de>
2018-04-23 14:11:08 +0200

    test file added

---
+++ ping-test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys, subprocess
+
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+
+HOSTS=sys.argv[1]
+MYIP=sys.argv[2]
+
+with open(HOSTS) as f:
+  content = f.readlines()
+  for l in content:
+    for ip in l.split(" "):
+      print(".. test host={0}".format(ip))
+      r = subprocess.call(["ssh", "ubuntu@{0}".format(ip), "ping -c 3 -i 5 {0} && iperf3 -c {0}".format(MYIP)])
+      break
